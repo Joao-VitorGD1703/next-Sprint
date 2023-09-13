29ce47d44f0fa82527da5e7e053ee5fc724f0755
João Vítor G Douverny <douvernyjoaovitor@gmail.com>
2023-09-12 22:41:45 -0300

    first-commit

---
+++ sprint-teste/src/components/Form/Form.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export const Form = () => {
+  return (
+    <div className=''>
+        <div className=''>
+            <input type="text" className='' />
+        </div>
+        <div>
+            <ul>
+                <li>
+                    <button></button>
+                </li>
+                <li>
+                    <button></button>
+                </li>
+                <li>
+                    <button></button>
+                </li>
+                <li>
+                    <button></button>
+                </li>
+                
+            </ul>
+        </div>
+    </div>
+  )
+}
